cb24c1b5e2e5bcb16c075fc382c98ac7e7cff874
Bastien Pognon <bastpognon@gmail.com>
2025-03-25 12:25:44 +0100

    feature : Saved data from input in locale storage

--- src/components/Expense.tsx
+++ src/components/Expense.tsx
@@ -1,7 +1,25 @@
 import { Button, NumberInput, TagsInput, TextInput } from "@mantine/core";
-import React from "react";
+import { useState } from "react";
 
 export const Expense: React.FC = () => {
+  const [dollars, setDollars] = useState<number | "">("");
+  const [description, setDescription] = useState<string>("");
+  const [categories, setCategories] = useState<string[]>([]);
+  const [count, setCount] = useState(Number(localStorage.getItem("count")) || 0);
+
+  const SaveData = () => {
+    if (dollars == "" || description == "" || categories.length == 0) {
+      alert("Please complete all fields in the form.");
+      return null;
+    }
+    localStorage.setItem(`${count}`, JSON.stringify({ dollars, description, categories }));
+    setCount(count + 1);
+    localStorage.setItem("count", JSON.stringify(count));
+    setDollars("");
+    setDescription("");
+    setCategories([]);
+  };
+
   return (
     <>
       <div>
@@ -13,14 +31,23 @@ export const Expense: React.FC = () => {
             prefix="$"
             decimalScale={2}
             thousandSeparator=","
+            value={dollars}
+            onValueChange={(values) => setDollars(values.floatValue ?? "")}
           />
-          <TextInput label="Description" placeholder="Add your description" />
+          <TextInput
+            label="Description"
+            placeholder="Add your description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
           <TagsInput
             label="Press Enter to submit a categories"
             placeholder="Categories"
             data={[]}
+            value={categories}
+            onChange={setCategories}
           />
-          <Button variant="filled" color="#fda5d5" className="mt-6">
+          <Button variant="filled" color="#fda5d5" className="mt-6" onClick={SaveData}>
             Button
           </Button>
         </div>
